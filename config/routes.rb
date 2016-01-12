3526a3c6a261711cbddbf9474cec73b0e39f7db8
nschumacher <nschuma@purdue.edu>
2016-01-12 12:40:52 -0800

    attempted contact form

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  match '/contacts', to: 'contacts#new', via: 'get'
+  resources "contacts", only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
