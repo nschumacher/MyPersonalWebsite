f760c33276fbb686e18d068c2b4113d14aa0b7d6
Nick Schumacher <nschuma@purdue.edu>
2016-07-30 23:51:26 -0400

    attempting to fix sign in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # for mailing and devise
-  config.action_mailer.default_url_options = { :host => 'nickschumacher.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'nickschumacher.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
