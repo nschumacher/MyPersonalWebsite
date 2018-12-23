a3172d5a58737b99dc75afe645768084a88f7a3a
Nick Schumacher <nschumacher@appirio.com>
2018-12-22 14:33:42 -1000

    Fixed CKEditor I believe.

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+
+////////////
+// CKEDITOR
+///////////
+//= link ckeditor/application.js
+//= link ckeditor/application.css
+//= link_tree ../javascripts/ckeditor
